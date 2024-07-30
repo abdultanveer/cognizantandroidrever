e0b2b808c54e6cb1c70de94787f4472f5955209e
abdul <abdul.tanveer@gmail.com>
2024-07-30 20:10:46 +0530

    start activities and pass data

--- app/src/main/java/com/example/cognizantrever/MainActivity.kt
+++ app/src/main/java/com/example/cognizantrever/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.cognizantrever
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +22,12 @@ class MainActivity : AppCompatActivity() {
         var nameEditText : EditText = findViewById(R.id.etName)
         var mainTextView : TextView = findViewById(R.id.tvMain)
 
-        var data = nameEditText.text.toString()
+        var data = nameEditText.text.toString();
         mainTextView.setText(data)
+
+        var hIntention = Intent(this,HomeActivity::class.java)
+        hIntention.putExtra("mykey",data)
+        startActivity(hIntention)
 
     }
 }
